1adc00abdaec7db7f4478e1384c2b53e4b04e64a
amir4atu <dormi31jake@mail.ru>
2026-04-27 10:02:24 +0500

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -14,6 +14,29 @@ public class MainTest {
     }
 }
 
-a = input()
-b = input()
-     print(a + b)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // массив өлшемі
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
